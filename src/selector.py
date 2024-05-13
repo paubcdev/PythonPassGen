c65d20b515313259827d2e29b15f6db5af36f995
pblanes <pau.blanes@asml.com>
2024-05-13 14:47:17 +0200

    selector checker redone

--- src/selector.py
+++ src/selector.py
@@ -10,6 +10,23 @@ def capitalizer_menu(password):
     print(capitalizer.capitalizer(password, cap_mode))
 
 
+def checker(answer):
+    check = 0
+    while check == 0:
+        if answer == 'y':
+            check = 1  # 1 for yes
+        elif answer == 'Y':
+            check = 1
+        elif answer == 'n':
+            check = 2  # 2 for no
+        elif answer == 'N':
+            check = 2
+        else:
+            print("Please select Y/N!")
+            check = 0
+        return check
+
+
 def menu():
     number_of_words = input("Introduce number of words for password: ")
     joiner = input("Introduce a joiner from this set: ( !@#$%^&*_-=+?.,~;: ) (or leave a space): ")
@@ -19,46 +36,9 @@ def menu():
     else:
         password_initial = generator.create_password_from_given_length(int(number_of_words), joiner)
 
-    checker = 0
-    while checker == 0:
-        capitalized = input("Do you want it capitalized? (Y/N): ")
-        if capitalized == 'y':
-            capitalizer_menu(password_initial)
-            checker = 1
-        elif capitalized == 'Y':
-            capitalizer_menu(password_initial)
-            checker = 1
-        elif capitalized == 'n':
-            print(password_initial)
-            checker = 1
-        elif capitalized == 'N':
-            print(password_initial)
-            checker = 1
-        else:
-            print("Please select Y/N!")
-            checker = 0
-
-    '''ent_done = 0
-    while ent_done == 0:
-        entropy = input("Do you want to know the entropy? (Y/N): ")
-
-        if entropy == 'Y':
-            ent_done = 1
-            pass
-
-        elif entropy == 'y':
-            ent_done = 1
-            pass
-
-        elif entropy == 'N':
-            ent_done = 1
-            pass
-
-        elif entropy == 'n':
-            ent_done = 1
-            pass
-
-        else:
-            ent_done = 0
-            return print("Error! Please select a correct option (Y/N).")
-'''
+    capitalized = input("Do you want it capitalized? (Y/N): ")
+    ans = checker(capitalized)
+    if ans == 1:  # 1 for yes
+        capitalizer_menu(password_initial)
+    elif ans == 2:  # 2 for no
+        print(password_initial)
